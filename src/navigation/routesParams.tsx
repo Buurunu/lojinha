fc32502b265f2d56cf3d09656c6b20a4463f8c7f
Gabriel <gabriel.oliver0033@gmail.com>
2024-12-13 11:10:51 -0300

    Atualização 2

--- src/navigation/routesParams.tsx
+++ src/navigation/routesParams.tsx
@@ -8,7 +8,7 @@ export type RootStackParamList = {
     NewProductScreen: undefined;
     PuchaseScreen: undefined;
     EditClientModal: { id: number }; // Parâmetro: ID do cliente
-    EditProductsModal: { product: Product }; // Produto passado como parâmetro
-    EditPurchaseModal: { purchase: Purchase };  // Purchase passado como parâmetro
+    EditProductsModal: { id: number }; // Parâmetro: ID do produto
+    EditPurchaseModal: { id: number };  // Parâmetro: ID da compra
   };
   
